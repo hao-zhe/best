9eb74148aef9c7d371ead403f95982b72f1f3e05
hao-zhe <3135524982@qq.com>
2021-02-05 09:20:13 +0000

    更改用户资料

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,4 +1,3 @@
-resources/views/shared/_user_info.blade.php
 <a href="{{ route('users.show', $user->id) }}">
   <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
 </a>
